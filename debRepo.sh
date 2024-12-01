293cf4c97a2e3155cdf014d0e18299d0b129d538
Daniel700HQ <59784927+Daniel700HQ@users.noreply.github.com>
2024-12-01 19:39:49 -0300

    Update debRepo.sh

--- debRepo.sh
+++ debRepo.sh
@@ -26,7 +26,7 @@ EOF
 echo "Actualizando la lista de paquetes e instalando actualizaciones..."
 apt update
 apt install -y xfce4 xfce4-goodies lightdm firefox-esr iptables iptables-persistent gnome-themes-extra network-manager network-manager-gnome wireguard wireguard-tools qrencode
-wget https://github.com/rustdesk/rustdesk/releases/download/1.3.3/rustdesk-1.3.3-x86_64.deb
+wget https://github.com/rustdesk/rustdesk/releases/download/1.3.3/rustdesk-1.3.3-x86_64.deb && apt install ./rustdesk-1.3.3-x86_64.deb
 
 # Preguntar si se desea crear un nuevo usuario
 echo "¿Quieres crear un nuevo usuario? (s/n): "
