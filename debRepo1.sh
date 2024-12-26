34f45f3be4b430a07b7c0a41bbde787ea6ddb8f5
Daniel700HQ <59784927+Daniel700HQ@users.noreply.github.com>
2024-12-26 02:01:30 -0300

    Update debRepo1.sh

--- debRepo1.sh
+++ debRepo1.sh
@@ -57,3 +57,63 @@ if [[ "$START_LIGHTDM" == "s" || "$START_LIGHTDM" == "S" ]]; then
 else
     echo "LightDM no se iniciará automáticamente."
 fi
+
+
+#!/bin/bash
+mkdir -p /etc/wireguard
+wg genkey | tee /etc/wireguard/server_private.key | wg pubkey > /etc/wireguard/server_public.key
+
+
+echo [Interface] >> /etc/wireguard/wg0.conf
+echo Address = 10.0.0.1/24 >> /etc/wireguard/wg0.conf
+echo ListenPort = 51820 >> /etc/wireguard/wg0.conf
+echo PrivateKey = $(cat "/etc/wireguard/server_private.key") >> /etc/wireguard/wg0.conf
+
+echo "PostUp = sysctl -w net.ipv4.ip_forward=1; iptables -A FORWARD -i wg0 -j ACCEPT; iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE" >> /etc/wireguard/wg0.conf
+echo "PostDown = sysctl -w net.ipv4.ip_forward=0; iptables -D FORWARD -i wg0 -j ACCEPT; iptables -t nat -D POSTROUTING -o eth0 -j MASQUERADE" >> /etc/wireguard/wg0.conf
+
+#!/bin/bash
+#CREAR CLIENTE  
+
+
+read -p "Introduce nombre de cliente: " NOMBRECLIENTE
+read -p "Introduce una IP Local wireguard que usara el cliente (ej 10.0.0.2/32) " IPCLIENTE
+read -p "Cual es el ip del servidor (142.132.12.23)" SERVERIP
+
+wg genkey | tee /etc/wireguard/$NOMBRECLIENTE-private.key | wg pubkey > /etc/wireguard/$NOMBRECLIENTE-public.key
+
+echo [Peer] >> /etc/wireguard/wg0.conf
+echo PublicKey = $(cat "/etc/wireguard/$NOMBRECLIENTE-public.key") >> /etc/wireguard/wg0.conf
+echo AllowedIPs = $IPCLIENTE >> /etc/wireguard/wg0.conf
+
+
+#creación del archivo conf del cliente
+echo [Interface] >> /etc/wireguard/$NOMBRECLIENTE.conf
+echo Address = $IPCLIENTE >> /etc/wireguard/$NOMBRECLIENTE.conf
+echo PrivateKey = $(cat "/etc/wireguard/$NOMBRECLIENTE-private.key") >> /etc/wireguard/$NOMBRECLIENTE.conf
+echo DNS = 1.1.1.1 >> /etc/wireguard/$NOMBRECLIENTE.conf
+
+echo [Peer] >> /etc/wireguard/$NOMBRECLIENTE.conf
+echo PublicKey = $(cat "/etc/wireguard/server_public.key") >> /etc/wireguard/$NOMBRECLIENTE.conf
+echo Endpoint = $SERVERIP:51820 >> /etc/wireguard/$NOMBRECLIENTE.conf
+echo AllowedIPs = 0.0.0.0/0 >> /etc/wireguard/$NOMBRECLIENTE.conf
+echo PersistentKeepalive = 25 >> /etc/wireguard/$NOMBRECLIENTE.conf
+
+#!/bin/bash
+# Activar la interfaz WireGuard
+wg-quick up wg0
+
+# Configurar las reglas de iptables
+iptables -A INPUT -p udp --dport 13231 -j ACCEPT
+iptables -A INPUT -s 10.0.0.0/24 -j ACCEPT
+iptables -t nat -A POSTROUTING -s 10.0.0.0/24 -j MASQUERADE
+
+#!/bin/bash
+# Desactivar la interfaz WireGuard
+wg-quick down wg0
+
+# Eliminar las reglas de iptables
+iptables -D INPUT -p udp --dport 13231 -j ACCEPT
+iptables -D INPUT -s 10.0.0.0/24 -j ACCEPT
+iptables -t nat -D POSTROUTING -s 10.0.0.0/24 -j MASQUERADE
+
